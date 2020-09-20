32a70d7045907e73800ffc085e246bdfc60c54b8
zahidulhaqueAI <zahidulhaquestar@gmail.com>
2020-09-20 10:30:11 +0530

    Thread.go

---
+++ Thread.go
@@ -0,0 +1,25 @@
+import (
+	"fmt"
+)
+
+// cerner_2^5_2020
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+
+func main() {
+	f("direct")
+	go f("goroutine")
+
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("Done")
+}
